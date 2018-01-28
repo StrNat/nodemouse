3a55acbba1e3b77cbe5d22ba0a0a14b29fbfafe8
starknighthood <nao1288stusj@gmail.com>
2018-01-28 14:28:43 +0900

    サンプルコードの修正

--- sample/switch.js
+++ sample/switch.js
@@ -2,8 +2,12 @@ let rpio = require('rpio');
 
 rpio.open(22, rpio.OUTPUT, rpio.LOW); //led0
 rpio.open(38, rpio.INPUT, rpio.PULL_DOWN); //sw0
-rpio.poll(38, (pin) => {
-    const status = !!!rpio.read(pin) ? rpio.HIGH : rpio.LOW;
+rpio.poll(38, (pin) => { // コールバック関数は’function’を省略したアロー関数式で表します。
+    const status = rpio.read(pin);
+    if (status === rpio.HIGH) {
+        rpio.write(22, rpio.LOW);
+    } else {
+        rpio.write(22, rpio.HIGH);
+    }
     console.log(status);
-    rpio.write(22, status);
 });
